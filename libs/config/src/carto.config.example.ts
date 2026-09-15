0d1270bab2e7ca7d861f1198c97eb072fbe22caa
wixhub <rublin@gmx.de>
2026-09-15 13:36:57 +0200

    chore: add documentation for CARTO_API_KEY in carto.config.example.ts

--- libs/config/src/carto.config.example.ts
+++ libs/config/src/carto.config.example.ts
@@ -1,1 +1,8 @@
+/**
+ * Shared CARTO API key used by applications that consume the map library.
+ *
+ * Note:
+ * Browser-side API keys are not secrets. If the key must be protected,
+ * it should not be embedded directly into frontend code.
+ */
 export const CARTO_API_KEY = '';
